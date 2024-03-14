546db097f957e74cda28c0c95e8dd5df531d74ca
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2024-03-14 11:35:51 +0700

    modulesQuill

---
+++ src/editor/moduleQuill.js
@@ -0,0 +1,5 @@
+const modulesEditor = {
+  toolbar: [['bold', 'italic', 'underline', 'strike', 'blockquote'], [{ list: 'ordered' }, { list: 'bullet' }], ['link']],
+};
+
+export default modulesEditor;
