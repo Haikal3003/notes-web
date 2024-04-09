00bb2ae6acf362c99e029ba06a433add70d67de4
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2024-04-09 16:19:33 +0700

    update/container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Container({ children }) {
   return (
-    <div id="container" className="relative w-full flex max-md:flex-wrap items-center">
+    <div id="container" className="relative w-full flex max-md:flex-wrap items-center ">
       {children}
     </div>
   );
--- src/components/PagesContainer.jsx
+++ src/components/PagesContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const PagesContainer = ({ children }) => {
-  return <div className="relative w-full ml-[130px] mr-[40px] my-6">{children}</div>;
+  return <div className="relative w-full ml-[130px] mr-[40px] my-6 max-sm:m-0 max-sm:mx-4 max-sm:my-4">{children}</div>;
 };
 export default PagesContainer;
